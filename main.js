ca4a4848ff9f47d4bcef116bd65b65fbaf10737a
alisongao0023 <76577180+alisongao0023@users.noreply.github.com>
2020-12-25 11:52:20 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function showMobileMenu(){
+    let element=document.querySelector("#mobile-menu");
+    element.style.display="block";
+}
+
+function closeMobileMenu(){
+    let element=document.querySelector("#mobile-menu");
+    element.style.display="none";
+}
